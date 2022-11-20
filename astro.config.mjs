82f69dfa9f43fdb7a210799f08ec24e4afb259da
flndr <thomas@flander.de>
2022-11-20 15:17:39 +0100

    from cloudflare to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,22 @@ import vue from '@astrojs/vue';
 import react from '@astrojs/react';
 import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), vue(), react(), storyblok({
-    accessToken: 'OsvNv534kS2nivAAj1EPVgtt',
-    components: {
-      page: 'storyblok/Page',
-      feature: 'storyblok/Feature',
-      grid: 'storyblok/Grid',
-      teaser: 'storyblok/Teaser',
-      vue_counter: 'storyblok/VueCounter',
-      svelte_counter: 'storyblok/SvelteCounter',
-      react_counter: 'storyblok/ReactCounter'
+    accessToken : 'OsvNv534kS2nivAAj1EPVgtt',
+    components  : {
+      page           : 'storyblok/Page',
+      feature        : 'storyblok/Feature',
+      grid           : 'storyblok/Grid',
+      teaser         : 'storyblok/Teaser',
+      vue_counter    : 'storyblok/VueCounter',
+      svelte_counter : 'storyblok/SvelteCounter',
+      react_counter  : 'storyblok/ReactCounter'
     }
   }), tailwind()],
-  output: "server",
-  adapter: cloudflare()
+  output  : "server",
+  adapter : vercel()
 });
